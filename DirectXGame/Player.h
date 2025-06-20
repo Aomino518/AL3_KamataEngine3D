d6cd023e48a02f2842b8f02d22cb787e06a4b466
MoriAoto <k024g0051@g.neec.ac.jp>
2025-06-20 11:02:02 +0900

    修正

--- DirectXGame/Player.h
+++ DirectXGame/Player.h
@@ -72,7 +72,7 @@ private:
 	// 重力加速度(下方向)
 	static inline const float kGravityAcceleration = 0.98f;
 	// 最大落下速度(下方向)
-	static inline const float kLimitFallSpeed = 0.3f;
+	static inline const float kLimitFallSpeed = 0.5f;
 	// ジャンプ初速(上方向)
 	static inline const float kJumpAcceleration = 20.0f;
 	//　マップチップによるフィールド
